deebf25769918e19cbcbd749b210c1a66ba897bc
Matias Andino <matiasandinoma@gmail.com>
2025-12-11 10:17:43 +0100

    WIP: estructura inicial de MissionTable y sistema de misiones

--- scripts/ui/mission_table/mission_table.gd
+++ scripts/ui/mission_table/mission_table.gd
@@ -3,9 +3,11 @@ extends CanvasLayer
 
 signal window_closed
 
-func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("ui_cancel"):
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel"):
 		close_window()
+
 
 func close_window() -> void:
 	get_tree().paused = false
